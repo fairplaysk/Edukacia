626b76c0786d749be25d664324cc8ba84ca36a8f
Michal Olah <olahmichal@gmail.com>
2010-12-16 21:00:24 +0100

    adding quizz listing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -70,17 +70,5 @@ module Admin
         end
       end
     end
-
-    # DELETE /users/1
-    # DELETE /users/1.xml
-    def destroy
-      @user = User.find(params[:id])
-      @user.destroy
-
-      respond_to do |format|
-        format.html { redirect_to(admin_users_url) }
-        format.xml  { head :ok }
-      end
-    end
   end
 end
--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
   def index
+    @quizzes = Quiz.all
   end
 end
