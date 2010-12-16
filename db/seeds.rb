0c47a20b532f46571a396ca58fff9d5e06954d1c
Michal Olah <olahmichal@gmail.com>
2010-12-16 20:38:08 +0100

    adding admin user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create(:name => 'admin', :password => 'changeme', :email => 'admin@kwizzer.sk')
