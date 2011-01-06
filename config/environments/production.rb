4dff0fb8ce250df00f1bf23d66860783cbb7fc6e
Michal Olah <olahmichal@gmail.com>
2011-01-06 13:34:21 +0100

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,11 +47,5 @@ Kwizzer::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  
-  config.action_mailer.smtp_settings = {  
-    :domain => "kvizy.fair-play.sk",
-    :address => "kvizy.fair-play.sk"
-  }
-  
   config.action_mailer.default_url_options = { :host => 'localhost' }
 end
